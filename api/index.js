b22999435baa20409200c76951ac47144466ad4e
mike <michaelboynton65@gmail.com>
2024-02-28 15:29:33 -0600

    added hash function to user passwords so they cannot be seen in the database

--- api/index.js
+++ api/index.js
@@ -3,9 +3,16 @@ const cors = require('cors');
 const app = express();
 const User = require('./models/User');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// hash user password when stored in database
+const salt = bcrypt.genSaltSync(10);
+
+
 
 app.use(cors());
 app.use(express.json());
+
 
 mongoose.connect('mongodb+srv://BobcatDrinkTracker:BobcatDrinkTracker@cluster0.2rbtkhh.mongodb.net/?retryWrites=true&w=majority');
 
@@ -16,12 +23,12 @@ app.post('/register', async (req, res) => {
         const userDoc = await User.create({
             email,
             username,
-            password
+            password:bcrypt.hashSync(password, salt)
         });
         res.status(200).json(userDoc); // Set status code to 200 explicitly
     } catch (error) {
         if (error.code === 11000) {
-            // Duplicate key error
+            // Duplicate key error -provided from mongoose
             if (error.keyPattern && error.keyValue) {
                 const duplicateKey = Object.keys(error.keyPattern)[0];
                 res.status(400).json({ error: `The ${duplicateKey} '${error.keyValue[duplicateKey]}' is already in use.` });
@@ -29,7 +36,7 @@ app.post('/register', async (req, res) => {
                 res.status(500).json({ error: 'Duplicate key error, but unable to determine the duplicate key.' });
             }
         } else {
-            // Other errors
+            // Other errors -
             res.status(500).json({ error: 'An unexpected error occurred.' });
         }
     }
