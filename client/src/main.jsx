6cdafa4169447c310edefb9fd54255d2617e22cb
mike <michaelboynton65@gmail.com>
2024-04-03 15:28:40 -0500

    User is now takendirectly to the login page when first getting on the website

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,37 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
-import Login from './login.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import Login from './login.jsx';
 import SignUp from './signup.jsx';
 import Dashboard from './dashboard';
 
-
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App/>
+    path: '/',
+    element: <Login /> // Set Login as the initial route
   },
   {
-    path: "/signup",
-    element: <SignUp/>
+    path: '/signup',
+    element: <SignUp />
   },
-    {
-    path: "/login",
-    element: <Login/>
-    },
-  
-    {
-      path: "/dashboard",
-      element: <Dashboard/>,
-    },
-
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
 ]);
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
